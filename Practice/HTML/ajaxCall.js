0ae8e8a423a9163279d37248f42e2711eef31ed1
Muhammad Azeem <m.azeemlaptop@gmail.com>
2024-03-17 02:02:17 +0500

    ajax calls post

--- Practice/HTML/ajaxCall.js
+++ Practice/HTML/ajaxCall.js
@@ -1,4 +1,4 @@
-
+// ----------------------------------Read Stories----------------------------------
 $(document).ready(function() {
     $.ajax({
         url: 'http://localhost:5000/books',
@@ -23,3 +23,27 @@ function displayStories(stories) {
         container.append(storyHtml);
     });
 }
+
+
+    // ----------------------------------post Stories----------------------------------
+    // postButton=$('#postStories')
+    function postStories(){
+        let title='My First Story post';
+        let story='a person try to post story and he posted successfully';
+        let image='https://images.pexels.com/photos/8058227/pexels-photo-8058227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+        let genres=['action','advanture','horror']
+        let level=10
+        let color='#1A6F45'
+        $.ajax({
+            url:'http://localhost:5000/books',
+            method:'POST',
+            data:{title, story, image, genres, level, color},
+            success:function(){
+                alert('Story posted successfully: '+ title)
+            },
+            error: function (error) {
+                console.error("Error creating story:", error);
+                alert("Failed to post story. Please try again.");
+            }
+        });
+    }
